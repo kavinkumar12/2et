0d33e9239483c871904c47fe7e4a3a689be668b5
kavinkumar12 <42169917+kavinkumar12@users.noreply.github.com>
2019-06-07 14:10:00 +0530

    Create qw46ergdsayhjsygogkoetykifarigfjk.py

---
+++ qw46ergdsayhjsygogkoetykifarigfjk.py
@@ -0,0 +1,8 @@
+import math
+import functools
+n,k=map(int,input().split())
+l=[int(i) for i in input().split()]
+for i in range(k):
+    a,b=map(int,input().split())
+    t=functools.reduce(math.gcd,l[a-1:b])
+    print(t)
