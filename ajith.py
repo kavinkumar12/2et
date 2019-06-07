49f8836cd521ab6447eb43145f1c0fdcdeec29c0
kavinkumar12 <42169917+kavinkumar12@users.noreply.github.com>
2019-06-07 12:43:39 +0530

    Create ajith.py

---
+++ ajith.py
@@ -0,0 +1,8 @@
+import sys,string,math
+b,a = input().split()
+b,a = int(b), int(a)
+L = [ int(x) for x in input().split()]
+for i in range(0,a) :
+   b,a = input().split()
+   b,a = int(a), int(a)
+   rint(sum(L[b-1:a]))
